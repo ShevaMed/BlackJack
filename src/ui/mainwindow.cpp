dd8010203a21eb78ca382b7cfd80573b8e397b69
ShevaMed <shevalenko87@gmail.com>
2023-06-18 14:53:49 +0300

    edit interface layout

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,15 +1,43 @@
-#include "mainwindow.h"
+#include "src/ui/mainwindow.h"
 #include "ui_mainwindow.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->coin20Button->setStyleSheet("image: url(:/images/coins/20.png); "
+                                    "background-color: transparent;");
+    ui->coin50Button->setStyleSheet("image: url(:/images/coins/50.png); "
+                                    "background-color: transparent;");
+    ui->coin100Button->setStyleSheet("image: url(:/images/coins/100.png); "
+                                    "background-color: transparent;");
+    ui->coin500Button->setStyleSheet("image: url(:/images/coins/500.png); "
+                                    "background-color: transparent;");
+    ui->coin1000Button->setStyleSheet("image: url(:/images/coins/1000.png); "
+                                    "background-color: transparent;");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_newGameButton_clicked()
+{
+    if (!game) {
+        game = std::make_unique<BlackjackGame>(1000);
+    }
+
+    ui->stackedWidget->setCurrentWidget(ui->placeBetPage);
+}
+
+
+void MainWindow::on_exitButton_clicked()
+{
+    std::exit(0);
 }
 
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -2,10 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "src/gamelogic/blackjackgame.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +17,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_newGameButton_clicked();
+
+    void on_exitButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    std::unique_ptr<BlackjackGame> game;
 };
 #endif // MAINWINDOW_H
